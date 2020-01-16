82eefc37314e2be85c995fc792fb304455c624a1
Jessica Wolff <jleahwolff@gmail.com>
2020-01-16 08:29:45 -0600

    Working in localStorage.

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export const useLocalStorage = (key, initialValue) => {
+
+const [ storedValue, setStoredValue ] = useState(() => {
+    if (localStorage.getItem(key)) {
+        return JSON.parse(localStorage.getItem(key));
+    } else {
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+    }
+});
+    // return [storedValue, setStoredValue];
+}
