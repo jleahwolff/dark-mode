57250efc2c10f82f21ad97bb88739715874be62e
Jessica Wolff <jleahwolff@gmail.com>
2020-01-16 10:50:02 -0600

    Done with localStorage.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,12 +5,14 @@ import ReactDOM from 'react-dom';
 export const useLocalStorage = (key, initialValue) => {
 
 const [ storedValue, setStoredValue ] = useState(() => {
-    if (localStorage.getItem(key)) {
-        return JSON.parse(localStorage.getItem(key));
-    } else {
-        localStorage.setItem(key, JSON.stringify(initialValue));
-        return initialValue;
-    }
+    const item = window.localStorage.getItem(key);
+    // if else statement
+    return item ? JSON.parse(item) : initialValue;
 });
-    // return [storedValue, setStoredValue];
-}
+const setValue = value => {
+    // saving the state to localStorage
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+};
+    return [storedValue, setValue];
+};
